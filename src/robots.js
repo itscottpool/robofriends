bfaa677ffca87665006c2eabff1a5839b73864d9
itscottpool <it.scottpool@gmail.com>
2019-03-24 23:50:09 -0400

    card components made

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Scott Pool',
+        username: 'Kiros_Xannon',
+        email: 'it.scottpool@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'John Allimonos',
+        username: 'TwilightReigns6',
+        email: 'jdallimonos6@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Stephen Pool',
+        username: 'WriterSP',
+        email: 'writersp@hotmail.com'
+    },
+    {
+        id: 4,
+        name: 'Kate Pool',
+        username: 'HeyItsKK',
+        email: 'kateveda@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tulip',
+        username: 'TheOncomingStorm',
+        email: 'flowers4buns@gmail.com'
+    }
+];
